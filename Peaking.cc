9a3c1145947edd944ddbf1cd54a083fe5b662105
mboretto <marco.bore@gmail.com>
2018-06-20 16:15:48 +0000

    Fix spectrum finder

--- Peaking.cc
+++ Peaking.cc
@@ -25,7 +25,7 @@ void Peaking()
     TF1* func[nAngle];
     TSpectrum* spettro = new TSpectrum(2*2); //2*n_picchi
     for(int i = 0; i < nAngle; i++){
-        vHisto[i] = new TH1F(Form("hHisto%d",i),";x (cm); counts",21,1.75,12.25);
+        vHisto[i] = new TH1F(Form("hHisto%d",i), ";x (cm); counts", 31, -0.25, 15.25);
     }
     float interval = 0.5; //cm
     float y0 = -6.8; //cm
@@ -37,27 +37,30 @@ void Peaking()
     std::vector<std::pair<long, int>> channel0, channel1;
     while (input_file1 >> coordinate_theta >> coordinate_y >> counts)
     {
-        int index = (int)coordinate_theta/40;
+        int index = (int) coordinate_theta / 40;
         if (index != prev_index){
-            int nfound = spettro->Search(vHisto[prev_index],2,"",0.1);
+            int nfound = spettro->Search(vHisto[prev_index], 1, "nobackground new", 0.0005);
+            std::cout << coordinate_theta << " Peaks found: " << nfound << std::endl;
             double* xpeaks = spettro->GetPositionX();
-            func[index] = new TF1(Form("func%d",index),"gaus(0)+gaus(3)",2,12);
-            func[index]->SetParLimits(2,.01,.8);
-            func[index]->SetParameter(2,0.5);
-            func[index]->SetParLimits(5,.01,.8);
-            func[index]->SetParameter(5,.5);
-            for (int p=0;p<nfound;p++) {
+            func[index] = new TF1(Form("func%d", index), "gaus(0)+gaus(3)", 2, 12);
+            func[index]->SetParLimits(2, .2, 2.0); //sigma
+            func[index]->SetParameter(2, 0.5); 
+            func[index]->SetParLimits(5, .2, 2.0); //sigma
+            func[index]->SetParameter(5, .5);
+            for (int p = 0; p < nfound; ++p) {
                 double xp = xpeaks[p];
+                std::cout << "Peaks found x: " << xp << std::endl;
                 int bin = vHisto[prev_index]->GetXaxis()->FindBin(xp);
                 double yp = vHisto[prev_index]->GetBinContent(bin);
-                func[index]->SetParameter(3*p,yp);
-                func[index]->SetParameter(3*p+1,xp);
+                func[index]->SetParameter(3 * p, yp); // Gaussian Nomalization
+                func[index]->SetParameter(3 * p + 1, xp); // Gaussian mean
             }
-            vHisto[prev_index]->Fit(Form("func%d",index));
+            vHisto[prev_index]->Fit(Form("func%d", index));
             vHisto[prev_index]->Write();
             prev_index=index;
         }
-        vHisto[index]->Fill(coordinate_y,counts);
+        vHisto[index]->Fill(coordinate_y, counts);
+        vHisto[index]->SetBinError(vHisto[index]->FindBin(coordinate_y),TMath::Sqrt(counts));
     }
 
     
